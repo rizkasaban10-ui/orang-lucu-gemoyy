afc465ccf18e5d80afc4b1f5e8d14ab33de41e03
RIZKANURUDDUHASABAN-121055520124123 <rizkanurudduha@gmail.com>
2025-12-15 22:45:11 +0900

    Add files via upload

---
+++ tugaspbo.py
@@ -0,0 +1,13 @@
+# Membuat variabel
+nama = "Aldy"
+umur = 19
+tinggi = 174
+
+# Menampilkan nilai variabel
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi, "cm")
+
+# Menggunakan variabel dalam operasi
+tahun_lahir = 2025 - umur
+print("Tahun lahir:", tahun_lahir)
